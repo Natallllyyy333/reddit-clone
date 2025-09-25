4f563f77f4c343e85d6117240a5013fe3202da72
Natallllyyy333 <natalllyyy333@gmail.com>
2025-09-25 22:04:09 +0200

    modify reddit_clone/urls.py

--- reddit_clone/urls.py
+++ reddit_clone/urls.py
@@ -17,10 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('posts/', include('posts.urls')), 
     path('register/', TemplateView.as_view(template_name='registration/register.html'), name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
