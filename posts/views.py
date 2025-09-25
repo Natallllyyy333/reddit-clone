a0bc0fdc715a1358b34d1f82ce49e56ae7cd3313
Natallllyyy333 <natalllyyy333@gmail.com>
2025-09-25 20:57:21 +0200

    add posts/ urls.py, modify templates/posts/list.htm, posts/admin.py, posts/views.py, reddit_clone/urls.py

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
+
+
+def post_list(request):
+    posts = Post.objects.all().order_by('-created_at')
+    return render(request, 'posts/list.html', {'posts': posts})
