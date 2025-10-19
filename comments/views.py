47adf770a15a2963bff6c79c11c890e6e298ba00
Natallllyyy333 <natalllyyy333@gmail.com>
2025-10-19 23:02:18 +0200

    edit comments/views.py

--- comments/views.py
+++ comments/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Comment
+from posts.models import Post
 
-# Create your views here.
+@login_required
+def add_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    
+    if request.method == 'POST':
+        content = request.POST.get('content')
+        parent_id = request.POST.get('parent_id')
+        
+        comment = Comment(
+            post=post,
+            author=request.user,
+            content=content
+        )
+        
+        if parent_id:
+            parent_comment = get_object_or_404(Comment, id=parent_id)
+            comment.parent = parent_comment
+        
+        comment.save()
+        
+        return redirect('post_detail', post_id=post_id)
+    
+    return redirect('post_detail', post_id=post_id)
+
+@login_required
+def delete_comment(request, comment_id):
+    comment = get_object_or_404(Comment, id=comment_id, author=request.user)
+    post_id = comment.post.id
+    comment.delete()
+    return redirect('post_detail', post_id=post_id)
