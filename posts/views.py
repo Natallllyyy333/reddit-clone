ae2b104b3c6958fcc1e69ae1eedede5c4cc05438
Natallllyyy333 <natalllyyy333@gmail.com>
2025-10-19 22:23:43 +0200

    edit posts/views.py

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Post
 from .forms import PostForm
+from django.http import JsonResponse
 from django.contrib.auth.decorators import login_required
 # Create your views here.
 
@@ -21,3 +22,26 @@ def create_post(request):
     else:
         form = PostForm()
     return render(request, 'posts/create.html', {'form': form})
+@login_required
+def vote_post(request, post_id, vote_type):
+    post = get_object_or_404(Post, id=post_id)
+    
+    if vote_type == 'upvote':
+        if request.user in post.downvotes.all():
+            post.downvotes.remove(request.user)
+        post.upvotes.add(request.user)
+    elif vote_type == 'downvote':
+        if request.user in post.upvotes.all():
+            post.upvotes.remove(request.user)
+        post.downvotes.add(request.user)
+    elif vote_type == 'remove_vote':
+        post.upvotes.remove(request.user)
+        post.downvotes.remove(request.user)
+    
+    post.save()
+    
+    return JsonResponse({
+        'score': post.score,
+        'upvotes': post.upvotes.count(),
+        'downvotes': post.downvotes.count()
+    })
