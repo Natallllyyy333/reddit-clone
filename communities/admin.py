b1707ddffb41c653852523275274bf1c52308a6f
Natallllyyy333 <natalllyyy333@gmail.com>
2025-10-20 10:51:14 +0200

    edit communities/admin.py

--- communities/admin.py
+++ communities/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Community
 
-# Register your models here.
+@admin.register(Community)
+class CommunityAdmin(admin.ModelAdmin):
+    list_display = ['name', 'created_by', 'created_at', 'member_count']
+    list_filter = ['created_at']
+    search_fields = ['name', 'description']
+    filter_horizontal = ['members', 'moderators']
+    
+    def member_count(self, obj):
+        return obj.member_count()
+    member_count.short_description = 'Members'
