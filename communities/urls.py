8040da917aba1cc05720f308951e6185fd4c6138
Natallllyyy333 <natalllyyy333@gmail.com>
2025-10-20 11:48:33 +0200

    add communities/urls.py

---
+++ communities/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+app_name = 'communities'
+
+urlpatterns = [
+    path('', views.community_list, name='community_list'),
+    path('create/', views.create_community, name='create_community'),
+    path('r/<str:community_name>/', views.community_detail, name='community_detail'),
+    path('r/<str:community_name>/join/', views.join_community, name='join_community'),
+    path('r/<str:community_name>/leave/', views.leave_community, name='leave_community'),
+]
