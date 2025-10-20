f5ff7d17d6deab408ce8c7021326d3e6002667ed
Natallllyyy333 <natalllyyy333@gmail.com>
2025-10-20 10:45:53 +0200

    edit communities/models.py

--- communities/models.py
+++ communities/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
 
-# Create your models here.
+class Community(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    description = models.TextField()
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    members = models.ManyToManyField(User, related_name='communities', blank=True)
+    moderators = models.ManyToManyField(User, related_name='moderated_communities', blank=True)
+    
+    def __str__(self):
+        return f"r/{self.name}"
+    
+    def member_count(self):
+        return self.members.count()
+    
+    def is_moderator(self, user):
+        return user in self.moderators.all()
+    
+    def get_absolute_url(self):
+        return reverse('community_detail', kwargs={'community_name': self.name})
