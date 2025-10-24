bcd32ecba1dec1e1fd4291f220c0696d1eaf00a1
Natallllyyy333 <natalllyyy333@gmail.com>
2025-10-24 12:04:30 +0200

    add share_modal.html, edit post_list.html, post_list.css, post_detail.html, urls.py

--- posts/urls.py
+++ posts/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from .views import PostListView, PostDetailView, PostCreateView, vote_post
 from . import views
 
+
+
 urlpatterns = [
     path('', PostListView.as_view(), name='post_list'),
     path('<int:pk>/', PostDetailView.as_view(), name='post_detail'),
