2a7f7cf97ffe98b86577b142fa9ddbc4354ec11f
Natallllyyy333 <natalllyyy333@gmail.com>
2025-10-24 17:09:17 +0200

    edit posts/forms.py, models.py

--- posts/forms.py
+++ posts/forms.py
@@ -3,30 +3,54 @@ from .models import Post
 from communities.models import Community
 
 class PostForm(forms.ModelForm):
+    media_files = forms.FileField(
+        required=False,
+        widget=forms.FileInput(attrs={
+            'class': 'form-control',
+            'multiple': True,  # Ключевое изменение - разрешаем множественный выбор
+            'accept': 'image/*,video/*'  # Уточняем типы файлов
+        })
+    )
     class Meta:
         model = Post
-        fields = ['title', 'content', 'community', 'media_file']
+        fields = ['title', 'content', 'community']
         widgets = {
             'title': forms.TextInput(attrs={
                 'class': 'form-control',
-                'placeholder': 'Введите заголовок вашего поста...'
+                'placeholder': 'Enter your post title...'
             }),
             'content': forms.Textarea(attrs={
                 'class': 'form-control',
-                'placeholder': 'Расскажите что-нибудь интересное...',
+                'placeholder': 'Tell something interesting...',
                 'rows': 6
             }),
             'community': forms.Select(attrs={
                 'class': 'form-select'
             }),
-            'media_file': forms.FileInput(attrs={
-                'class': 'form-control'
-            })
+           
         }
     
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['community'].queryset = Community.objects.all()
         self.fields['community'].required = False
-        self.fields['community'].empty_label = "Выберите сообщество (необязательно)"
+        self.fields['community'].empty_label = "Pick a community (not required)"
         self.fields['community'].label_from_instance = lambda obj: obj.name
+    
+
+    def save(self, commit=True):
+        # Сначала сохраняем пост
+        post = super().save(commit=commit)
+        
+        # Обрабатываем множественные файлы
+        media_files = self.files.getlist('media_files')
+        for media_file in media_files:
+            if media_file:  # Проверяем что файл не пустой
+                # Создаем запись PostMedia для каждого файла
+                from .models import PostMedia  # Импортируем здесь чтобы избежать циклического импорта
+                PostMedia.objects.create(
+                    post=post,
+                    media_file=media_file
+                )
+        
+        return post
--- posts/models.py
+++ posts/models.py
@@ -44,7 +44,24 @@ class Post(models.Model):
         choices=MEDIA_TYPE_CHOICES,
         default='none'
     )
+
+    @property
+    def first_media(self):
+        """Возвращает первый медиафайл для обратной совместимости"""
+        if self.media_files.exists():
+            return self.media_files.first()
+        # Для старых постов с одним файлом
+        if self.media_file:
+            return self
+        return None
     
+    @property
+    def has_multiple_media(self):
+        """Проверяет есть ли множественные медиафайлы"""
+        return self.media_files.count() > 1
+    
+
+
     def __str__(self):
         return self.title
     
@@ -74,6 +91,13 @@ class Post(models.Model):
         
         super().save(*args, **kwargs)
 
+        if self.media_file and not self.media_files.exists():
+            PostMedia.objects.create(
+                post=self,
+                media_file=self.media_file,
+                media_type=self.media_type
+            )
+
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='post_comments')
@@ -95,3 +119,32 @@ class Share(models.Model):
 
     class Meta:
         unique_together = ['post', 'user']
+
+
+class PostMedia(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='media_files')
+    media_file = models.FileField(
+        upload_to='post_media/%Y/%m/%d/',
+        validators=[FileExtensionValidator(
+            allowed_extensions=['jpg', 'jpeg', 'png', 'gif', 'mp4', 'mov', 'avi']
+        )]
+    )
+    media_type = models.CharField(
+        max_length=10,
+        choices=Post.MEDIA_TYPE_CHOICES,  # Используем те же choices что и у Post
+        default='none'
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def save(self, *args, **kwargs):
+        # Автоматически определяем тип медиа
+        if self.media_file:
+            file_extension = self.media_file.name.lower().split('.')[-1]
+            if file_extension in ['jpg', 'jpeg', 'png', 'gif']:
+                self.media_type = 'image'
+            elif file_extension in ['mp4', 'mov', 'avi']:
+                self.media_type = 'video'
+        super().save(*args, **kwargs)
+    
+    def __str__(self):
+        return f"Media for {self.post.title}"
